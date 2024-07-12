f1ad079f45b739e3d918f00e2a1ea47e90589885
PriyankaMobrilz <priyanka@mobrilz.com>
2024-07-12 16:32:28 +0530

    Update DriverSignupController.php

--- app/Http/Controllers/Api/V1/Auth/Registration/DriverSignupController.php
+++ app/Http/Controllers/Api/V1/Auth/Registration/DriverSignupController.php
@@ -173,7 +173,6 @@ class DriverSignupController extends LoginController
         if($request->has('is_bid_app')){
             $data['is_bid_app']=1;
         }
-
         $user = $this->user->create($data);
 
         $created_params['company_key'] = $company_key;
@@ -194,7 +193,7 @@ class DriverSignupController extends LoginController
         }
 
         // // Store records to firebase
-        $this->database->getReference('drivers/'.'driver_'.$driver->id)->set(['id'=>$driver->id,'vehicle_type'=>$request->input('vehicle_type'),'active'=>1,'updated_at'=> Database::SERVER_TIMESTAMP]);
+        $dat = $this->database->getReference('drivers/'.$driver->id)->set(['id'=>$driver->id,'vehicle_type'=>$request->input('vehicle_type'),'active'=>1,'updated_at'=> Database::SERVER_TIMESTAMP]);
 
         $driver_detail_data = $request->only(['is_company_driver','company']);
         $driver_detail = $driver->driverDetail()->create($driver_detail_data);
