c70c3316c76555f9aa31327ed2d7f7202316ee0c
PriyankaMobrilz <priyanka@mobrilz.com>
2024-08-07 12:52:16 +0530

    vehicle fetch according company_id and service_location_id

--- routes/api/v1/driver.php
+++ routes/api/v1/driver.php
@@ -38,6 +38,5 @@ Route::prefix('driver')->namespace('Driver')->middleware('auth')->group(function
         Route::post('send','ChatController@send');
         Route::post('seen','ChatController@updateSeen');
 
-        Route::get('types/{company_id}', 'VehicleTypeController@getVehicleTypesByCompanyId');
     });
 });
